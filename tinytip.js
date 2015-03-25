1c5f386cce3085a2d50f0db5466f9da0acd03153
mamod <mamod.mehyar@gmail.com>
2015-03-25 22:23:00 +0300

    v0.0.2

--- tinytip.js
+++ tinytip.js
@@ -3,33 +3,11 @@
 * MIT Licensed
 * @author Mamod Mehyar
 * http://sweefty.com
-* version : 0.0.1
+* version : 0.0.2
 */
 
 (function($){
     "use strict";
-    var _preventOnMouseEnter = false;
-    var _SW_tooltip,_SW_tooltipClass;
-    $(document).ready(function(){
-        //initiate tooltip div
-        _SW_tooltip = $('<div class="tinytip"></div>').css({
-            width : 'auto',
-            'max-width' : '90%',
-            position : 'absolute',
-            display : 'none',
-            top : '0',
-            left : '0',
-            margin: '0',
-            zIndex : '99999'
-        }).appendTo('body');
-
-        _SW_tooltip.on('mouseenter', function(){
-            _preventOnMouseEnter = true;
-        }).on('mouseleave', function(){
-            _preventOnMouseEnter = false;
-        });
-    });
-    
     var tooltip_options = {
         position : 'top',
         speed : 250,
@@ -39,18 +17,36 @@
     };
 
     var lastElement;
-    var _isview = false;
+    
 
     var _tooltip = function(ele,obj){
+        var _isview = false;
         obj = $.extend({},tooltip_options, obj);
         var text = obj.tooltip || ele.data('tooltip');
-        var fix = obj.fix || '0 0';
+        var fix = obj.fix || {top: 0, left: 0};
         
+        var _preventOnMouseEnter = false;
+        var _SW_tooltip = $('<div class="tinytip"></div>').css({
+            width : 'auto',
+            position : 'absolute',
+            display : 'none',
+            top : '0',
+            left : '0',
+            margin: '0',
+            zIndex : '99999'
+        }).appendTo('body').on('mouseenter', function(){
+            _preventOnMouseEnter = true;
+        }).on('mouseleave', function(){
+            _preventOnMouseEnter = false;
+        });
+
+        if (obj.addClass){
+            _SW_tooltip.addClass(obj.addClass);
+        }
         if (text || obj.content){
-            var fixcoords = fix.split(' '),
-            fixX = parseInt(fixcoords[0]),
-            fixY = parseInt(fixcoords[1]);
-            var _class = obj.addClass;
+            var fixX = parseInt(fix.left) || 0,
+                fixY = parseInt(fix.top) || 0;
+
             var position = obj.position || 'top';
             var speed = parseInt(obj.speed);
             var startX = function(x){
@@ -60,25 +56,15 @@
             };
             
             if (obj.animation){
-                var xy = obj.animation.split('+');
-                if (xy.length == 2){
-                    xy.push('+');
-                } else {
-                    xy = [];
-                    xy = obj.animation.split('-');
-                    xy.push('-');
-                }
-                
-                var val = parseInt(xy[1]);
-                if (xy[0] == 'left'){
+                if (obj.animation.left){
                     startX = function(x){
-                        //var val = parseInt(xy[1]);
-                        var newx = (xy[2] == '-') ? x-val : x+val;
+                        var newx = x + obj.animation.left;
                         return newx;
                     };
-                } else {
+                }
+                if (obj.animation.top){
                     startY = function(y){
-                        var newy = (xy[2] == '-') ? y-val : y+val;
+                        var newy = y+obj.animation.top;
                         return newy;
                     };
                 }
@@ -86,16 +72,17 @@
             
             var start1,start2;
             var closeTooltip = function(e){
+                if (!_isview && obj.on === obj.off){
+                    return false;
+                }
+
                 e.preventDefault();
                 setTimeout(function(){
                     if (_preventOnMouseEnter && obj.preventClose){
                         closeTooltip(e); 
-                        return; 
+                        return;
                     }
 
-                    if (!_isview){ return false; }
-
-                    //e.preventDefault();
                     _SW_tooltip.stop().animate({
                         opacity: 0,
                         top:start1,
@@ -115,23 +102,14 @@
 
             var viewTooltip = function(e){
                 if (e){ e.preventDefault(); }
-                if (_isview && obj.on === obj.off){
-                    if (lastElement === ele){
+                
+                if (_isview){
+                    if (obj.on === obj.off){
                         closeTooltip(e);
-                        return;
-                    } else {
-                        _isview = false;
                     }
-                } else if (_isview && lastElement === ele){
-                    return;
+                    return false;
                 }
 
-                lastElement = ele;
-                if (_SW_tooltipClass){
-                    _SW_tooltip.removeClass(_SW_tooltipClass);
-                }
-                
-                _SW_tooltipClass = _class;
                 if (obj.content){
                     obj.clone = obj.content.clone();
                     _SW_tooltip.html(obj.clone);
@@ -161,7 +139,8 @@
                 
                 start1 = parseFloat(startY(top));
                 start2 = parseFloat(startX(left));
-                _SW_tooltip.addClass(_class).stop().css({
+                
+                _SW_tooltip.stop().css({
                     top : start1,
                     left : start2,
                     opacity: 0,
@@ -174,11 +153,13 @@
                 });
             };
 
-            ele.on(obj.on, function(e){
-                viewTooltip(e);
-            }).on( obj.off, function(e){
-                closeTooltip(e);
-            });
+            if (obj.on !== false){
+                ele.on(obj.on, function(e){
+                    viewTooltip(e);
+                }).on(obj.off, function(e){
+                    closeTooltip(e);
+                });
+            }
         }
     };
     
@@ -191,10 +172,15 @@
         
         var options = $.extend({}, tooltip_options, customOptions);
         options.tooltip = text;
+        if (typeof options.tooltip === 'object'){
+            options.content = options.tooltip;
+            options.content.hide();
+        }
+
         return this.each(function(){
             var $this = $(this);
             if ($this[0] == $(document)[0]){
-                tooltip_options = $.extend({}, tooltip_options, text);
+                tooltip_options = $.extend({}, tooltip_options, customOptions);
             } else {
                 _tooltip($this, options);
             }
